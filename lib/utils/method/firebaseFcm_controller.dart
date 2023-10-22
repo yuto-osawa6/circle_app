28e3fafb17b80f2e30610cda4afe825f484e63fd
yuto-osawa6 <62257152+yuto-osawa6@users.noreply.github.com>
2023-10-23 00:00:02 +0900

    Update firebaseFcm_controller.dart

--- lib/utils/method/firebaseFcm_controller.dart
+++ lib/utils/method/firebaseFcm_controller.dart
@@ -4,7 +4,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 class NotificationHandlers {
   static void initFirebaseMessaging() {
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print("フォアグランド");
+      // print("フォアグランド");
       print(message);
       NotificationHandlers.handleMessage(message); // クラス名で直接呼び出し
     });
