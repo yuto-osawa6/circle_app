12b3d7fec01ac3710a0cd6dacade930a783b901f
yuto-osawa6 <62257152+yuto-osawa6@users.noreply.github.com>
2023-11-30 22:02:21 +0900

    あ

--- lib/client/dio/dio.dart
+++ lib/client/dio/dio.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 // import 'package:dio/adapter.dart';
 // import 'package:dio/dio.dart';
